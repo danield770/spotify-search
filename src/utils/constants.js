95488ee959a7cbe9b90885f2847e594f423d7dbe
danield770 <dddavid2@gmail.com>
2021-09-26 10:23:58 +0300

    modify redirect url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
 export const constants = {
   REACT_APP_AUTHORIZE_URL: 'https://accounts.spotify.com/authorize',
-  REACT_APP_REDIRECT_URL: 'http://localhost:3000/callback',
+  REACT_APP_REDIRECT_URL: 'https://spotifunix.herokuapp.com/callback',
   REACT_APP_CLIENT_ID: '405281f84e1b47808864d0cbf83a9c1e',
 };
